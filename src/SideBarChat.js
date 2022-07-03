1f3e9f639472e94024e3caff17c641d8e524ff6e
ashokyemula <ashokyemula@gmail.com>
2022-07-03 12:42:08 -0400

    changed sidebarchat js july 3 2022

--- src/SideBarChat.js
+++ src/SideBarChat.js
@@ -1,73 +1,66 @@
 import React, { useEffect, useState } from 'react'
 import './SideBarChat.css';
 import { Avatar } from "@material-ui/core";
-// import axios from 'axios';
+import { PostAdd } from '@material-ui/icons';
+import axios from 'axios';
 
 
 
-class SideBarChat extends React.Component { 
-
-  state = {
-    roomName: ''
-  }; 
-
-  const roomName = prompt("Please Enter Chat Room Name");
-
-            render () {
-
-              
-
-            if (roomName) {
-              
-                 console.log(roomName);
-                this.setstate( {roomName: roomName});
 
 
-                
-                    const response =  fetch('/createRoom', {
-                         method: 'POST',
-                         headers: {
-                           'Content-Type': 'application/json',
-                         },
-                         body: JSON.stringify({ roomName: this.state.roomName }),
-                       });
-                       return (
-                        <div className="sidebarChat">
+function SideBarChat({addNewChat}) { 
+
+  const [seed, setSeed] = useState("");
+
   
-                          <div className="sidebarChat__info">
-                            <h2>{roomName}</h2>
-                            <p>Last message...</p>
-                           </div>
-                        </div>
-                       )
-                // this is test for github
 
-
-
-            }
+  const createChat = () => {
+    const roomName = prompt("Please Enter Chat Room Name");
+    const onSubmit = rname => { 
+      axios.post('http://localhost:5000/createRoom', rname)
+      .then((res) => {
+          if (res.status == 200) {
+            alert("room created")
+          } else {
+                Promise.reject()
           }
-    };
-
-    // useEffect(()=>
-    //             {
-    //                setSeed( Math.floor(Math.random() * 5000));
-    //             }, [])
+      })
+    }
 
     
-  // return !addNewChat ? (
-  //   <div className="sidebarChat">
-  //       <Avatar className="Avatar" src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
-  //       <div className="sidebarChat__info">
-  //           <h2>{name}</h2>
-  //           <p>Last message...</p>
-  //       </div>
-  //   </div>
-  // ) : (
+    if (roomName) {
+      //do some database stuff here
+      // fetch('http://localhost:5000/createRoom', {
+        method: POST,
+        headers: {
+          'content-type': 'application/json'
+        },
+          body: {roomName:roomName}
+        
 
-  //   <div onClick = {createChat} className="sidebarChat">
-  //     <h2>Add New Chat</h2>
-  //   </div>
-  // )
+      }).then(()=>console.log('createChat api called'))
+    }
+  };
 
+   useEffect(()=> {
+            setSeed( Math.floor(Math.random() * 5000));
+    }, [])
 
-export default SideBarChat
+    
+  return !addNewChat ? (
+     <div className="sidebarChat">
+        <Avatar className="Avatar" src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+         <div className="sidebarChat__info">
+           <h2>Room Name</h2>
+            <p>Last message...</p>
+         </div>
+    </div>
+   ) : (
+
+     <div onClick = {createChat} className="sidebarChat">
+       <h2>Add new Chat</h2>
+     </div>
+  )
+
+}
+export default SideBarChat;
